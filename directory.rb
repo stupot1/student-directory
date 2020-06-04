03a30e6e3bf65fe6407af60971fe6aa17bad0f62
stupot1 <scpitc@gmail.com>
2020-06-04 14:49:58 +0000

    Step 13 - Added command line file argument

--- directory.rb
+++ directory.rb
@@ -4,9 +4,10 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
+
 def process(selection)
   case selection
     when "1"
@@ -23,6 +24,7 @@ def process(selection)
       puts "I don't know what you meant, try again"
   end
 end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -30,25 +32,28 @@ def print_menu
   puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
+
 def show_students
   print_header
   print_students_list
   print_footer
 end
+
 def input_students
   puts "Please enter the names of the students"
   puts  "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student to the hash array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end 
 end 
+
 def save_students
   #open file for writing
   file = File.open("students.csv", "w")
@@ -60,25 +65,42 @@ def save_students
   end
   file.close
 end 
-def load_students
-  file = File.open("students.csv", "r")
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end 
   file.close
 end 
+
+def try_load_students
+  filename = ARGV.first # first argument from command line
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} deosnt exist."
+    exit
+  end 
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------" 
 end
+
 def print_students_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer
   puts "Overall, we have #{@students.count} great students"  
 end 
 #nothing happens until we call the methods
+try_load_students
 interactive_menu
