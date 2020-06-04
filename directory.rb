9ceb2630075caab573f45ef269e863820d18f6ad
stupot1 <scpitc@gmail.com>
2020-06-04 10:51:09 +0000

    Step 5 Commit

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 # let's put all students into an array
 students = [
   "Dr. Hannibal Lecter",
@@ -13,19 +12,19 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-# finally, we print the total
-puts "Overall, we have #{student_count} great students"
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------" 
+end
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"  
+end 
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
